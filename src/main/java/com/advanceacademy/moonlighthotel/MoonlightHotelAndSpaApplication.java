4f2060e6a89fa329694c98d2972503e47725235f
akkrastevak <96346844+akkrastevak@users.noreply.github.com>
2023-08-18 19:44:07 +0300

    initial commit with project base (#1)

---
+++ src/main/java/com/advanceacademy/moonlighthotel/MoonlightHotelAndSpaApplication.java
@@ -0,0 +1,13 @@
+package com.advanceacademy.moonlighthotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonlightHotelAndSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonlightHotelAndSpaApplication.class, args);
+	}
+
+}
