b7b254a973042f3d2ce186ac66357faaa3d9315a
Stiliyanov <133772284+Stiliyanov@users.noreply.github.com>
2023-08-24 11:01:07 +0300

    Jb2 at1 6 create contact us form entity (#7)
    * my first commit
    
    * test commit
    
    * new
    
    * Alex test
    
    * New commit
    
    * Updated constraints to follow the ones from User class except for phone regex (not working) and improved message field capacity.
    
    * add column name
    
    * change package name
    
    * Jb2 at1 2 create user and user role entity (#4)
    
    * JB2AT1-2 Create User and UserRole entities with validation
    
    * Create all classes connected with restaurant part (#5)
    
    * Create all classes connected with restaurant part
    
    * Add Github CI script for deploying on DEV server from main branch (#9)
    
    * Update table names
    
    * Update table names
    
    * change package name
    
    * Update properties file
    
    * New commit
    
    * change package name
    
    ---------
    
    Co-authored-by: dermenji <alex.dermenji@gmail.com>
    Co-authored-by: akkrastevak <96346844+akkrastevak@users.noreply.github.com>
    Co-authored-by: adoncheva <annadoncheva@yahoo.com>
    Co-authored-by: Mikhail Kyosev <mikhail.kyosev@gmail.com>

---
+++ src/main/java/com/advanceacademy/moonlighthotel/entity/ContactUsForm.java
@@ -0,0 +1,42 @@
+package com.advanceacademy.moonlighthotel.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Entity
+@Table(name = "contact_us")
+public class ContactUsForm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(name = "user_name")
+    @NotNull
+    @Size(min = 2, max = 50, message = "User name must be between {min} and {max} characters")
+    private String userName;
+
+
+    @NotNull
+    @Email(regexp = "^[^ @]+@[^ @]+\\.[^ @]+$", message = "Invalid email address format")
+    @Column(name = "user_email")
+    private String userEmail;
+
+    @NotNull
+    @Pattern(regexp = "^(?:00|\\+)[0-9\\s.-]{6,20}$", message = "Invalid phone number format")
+    @Column(name = "user_phone_number", length = 30)
+    private String userPhone;
+
+    @NotNull
+    @NotBlank(message = "Message may not be blank")
+    private String userMessage;
+
+}
