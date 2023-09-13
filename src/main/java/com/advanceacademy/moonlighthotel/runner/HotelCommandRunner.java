17724f6957b1a44ea910735e6cbfdc80cafd27b3
BorisssIliev <133894994+BorisssIliev@users.noreply.github.com>
2023-09-13 14:00:05 +0300

    Jb2 at1 25 add assets for hotel rooms (#24)
    * assets for the hotel are done
    
    * assets for the hotel are done
    
    * JB2AT1-16-Add-service-layed-for-Conract-Form: Added service layer, dt… (#22)
    
    * JB2AT1-16-Add-service-layed-for-Conract-Form: Added service layer, dto and service-layer tests; Added new code in repository class as there was missing code in it on the main remote branch; Used last pushed contact-us-form entity with included @Column(columnDefinition="TEXT").
    
    * JB2AT1-16-Add-service-layed-for-Conract-Form: Added converter and exception package and class to make it runnable.
    
    * JB2AT1-20-Add-service-layer-for-Car-and-related-Restaurant-and-all-releted version1 (#21)
    
    * Adjust imports
    
    * JB2AT1-21 create service package (#19)
    
    * JB2AT1-21 create service package
    
    * JB2AT1-21 create service and implementation
    
    * JB2AT1-21 testing the table reservation
    
    * JB2AT1-21 testing the  restaurant service
    
    * JB2AT1-21 testing the  restaurant service
    
    * JB2AT1-21 test the restaurant services
    
    * JB2AT1-21 update the restaurant services test
    
    * JB2AT1-21 update restaurant services test
    
    * JB2AT1-21 update the reservation services test
    
    * JB2AT1-21 update the pom xml
    
    * JB2AT1-21 fixed the contact us from repository
    
    * Added repository layer for Car and all related. Used findByCarsId, as… (#18)
    
    * Added repository layer for Car and all related. Used findByCarsId, as it was Car cars in class CarTransfer in main.
    
    * Removed maven compiler plugin in pom.
    
    * HOT fix - app failure
    
    * service layer for user and roles done (#20)
    
    * service layer for user and roles done
    
    * Rebase with hotfix and adjust packages
    
    * Some fixes
    
    ---------
    
    Co-authored-by: dermenji <alex.dermenji@gmail.com>
    
    * JB2AT1-16-Add-service-layed-for-Conract-Form: Added service layer, dt… (#22)
    
    * JB2AT1-16-Add-service-layed-for-Conract-Form: Added service layer, dto and service-layer tests; Added new code in repository class as there was missing code in it on the main remote branch; Used last pushed contact-us-form entity with included @Column(columnDefinition="TEXT").
    
    * JB2AT1-16-Add-service-layed-for-Conract-Form: Added converter and exception package and class to make it runnable.
    
    * JB2AT1-20-Add-service-layer-for-Car-and-related-Restaurant-and-all-releted version1 (#21)
    
    * Adjust imports
    
    * JB2AT1-21 rebase and fixed the restaurant tests
    
    * JB2AT1-21 fixed tests
    
    ---------
    
    Co-authored-by: Stiliyanov <133772284+Stiliyanov@users.noreply.github.com>
    Co-authored-by: dermenji <alex.dermenji@gmail.com>
    Co-authored-by: BorisssIliev <133894994+BorisssIliev@users.noreply.github.com>
    Co-authored-by: adoncheva <annadoncheva@yahoo.com>
    
    * Jb2 at1 18 add service layer for hotel and all related (#25)
    
    * JB2AT1-18 added service layer for hotel, added unit tests for service, add exceptions and format code on RoomReservation
    
    * JB2AT1-18 added service layer for hotel, added unit tests for service, add exceptions and format code on RoomReservation
    
    * JB2AT1-18 added service layer for hotel, added unit tests for service, add exceptions and format code on RoomReservation
    
    * assets for the hotel are done 2
    
    * assets for the hotel are done 3 and have duplications check
    
    ---------
    
    Co-authored-by: Stiliyanov <133772284+Stiliyanov@users.noreply.github.com>
    Co-authored-by: adoncheva <annadoncheva@yahoo.com>
    Co-authored-by: dermenji <alex.dermenji@gmail.com>
    Co-authored-by: Ayhan Krala <codewolf27@gmail.com>
    Co-authored-by: akkrastevak <96346844+akkrastevak@users.noreply.github.com>

---
+++ src/main/java/com/advanceacademy/moonlighthotel/runner/HotelCommandRunner.java
@@ -0,0 +1,241 @@
+package com.advanceacademy.moonlighthotel.runner;
+
+import com.advanceacademy.moonlighthotel.entity.hotel.Room;
+import com.advanceacademy.moonlighthotel.entity.hotel.RoomType;
+import com.advanceacademy.moonlighthotel.entity.hotel.RoomView;
+import com.advanceacademy.moonlighthotel.repository.hotel.RoomRepository;
+import com.advanceacademy.moonlighthotel.service.hotel.impl.RoomServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class HotelCommandRunner implements CommandLineRunner {
+
+    @Autowired
+    RoomRepository roomRepository;
+
+    @Autowired
+    RoomServiceImpl roomService;
+
+    public boolean isRoomNumberUnique(Room actual) {
+        List<Room> allByRoomNumber = roomRepository.findAll();
+
+        boolean isUnique = allByRoomNumber.stream()
+                .noneMatch(room -> room.getRoomNumber().equals(actual.getRoomNumber()));
+
+        return isUnique;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+
+        //Standard rooms
+        //Sea Standard rooms
+        Room standardSeaRoom1 = Room.builder()
+                .roomNumber(12)
+                .roomType(RoomType.STANDART)
+                .roomView(RoomView.SEA)
+                .roomPrice(220.00)
+                .build();
+
+
+        if (isRoomNumberUnique(standardSeaRoom1)) {
+            roomService.createRoom(standardSeaRoom1);
+        }
+
+
+        Room standardSeaRoom2 = Room.builder()
+                .roomNumber(13)
+                .roomType(RoomType.STANDART)
+                .roomView(RoomView.SEA)
+                .roomPrice(220.00)
+                .build();
+
+        if (isRoomNumberUnique(standardSeaRoom2)) {
+            roomService.createRoom(standardSeaRoom2);
+        }
+
+        //Standard Pool rooms
+        Room standardPoolRoom1 = Room.builder()
+                .roomNumber(14)
+                .roomType(RoomType.STANDART)
+                .roomView(RoomView.POOL)
+                .roomPrice(220.00)
+                .build();
+
+        if (isRoomNumberUnique(standardPoolRoom1)) {
+            roomService.createRoom(standardPoolRoom1);
+        }
+
+        Room standardPoolRoom2 = Room.builder()
+                .roomNumber(15)
+                .roomType(RoomType.STANDART)
+                .roomView(RoomView.POOL)
+                .roomPrice(220.00)
+                .build();
+
+        if (isRoomNumberUnique(standardPoolRoom2)) {
+            roomService.createRoom(standardPoolRoom2);
+        }
+
+
+        //Standard Garden rooms
+        Room standardGardenRoom1 = Room.builder()
+                .roomNumber(16)
+                .roomType(RoomType.STANDART)
+                .roomView(RoomView.GARDEN)
+                .roomPrice(220.00)
+                .build();
+
+        if (isRoomNumberUnique(standardGardenRoom1)) {
+            roomService.createRoom(standardGardenRoom1);
+        }
+
+        Room standardGardenRoom2 = Room.builder()
+                .roomNumber(17)
+                .roomType(RoomType.STANDART)
+                .roomView(RoomView.GARDEN)
+                .roomPrice(220.00)
+                .build();
+
+        if (isRoomNumberUnique(standardGardenRoom2)) {
+            roomService.createRoom(standardGardenRoom2);
+        }
+
+        Room standardGardenRoom3 = Room.builder()
+                .roomNumber(18)
+                .roomType(RoomType.STANDART)
+                .roomView(RoomView.GARDEN)
+                .roomPrice(220.00)
+                .build();
+
+        if (isRoomNumberUnique(standardGardenRoom3)) {
+            roomService.createRoom(standardGardenRoom3);
+        }
+
+        Room standardGardenRoom4 = Room.builder()
+                .roomNumber(19)
+                .roomType(RoomType.STANDART)
+                .roomView(RoomView.GARDEN)
+                .roomPrice(220.00)
+                .build();
+
+        if (isRoomNumberUnique(standardGardenRoom4)) {
+            roomService.createRoom(standardGardenRoom4);
+        }
+
+
+        //Studios
+        //Sea Studios
+        Room seaStudio1 = Room.builder()
+                .roomNumber(22)
+                .roomType(RoomType.STUDIO)
+                .roomView(RoomView.SEA)
+                .roomPrice(320.00)
+                .build();
+
+        if (isRoomNumberUnique(seaStudio1)) {
+            roomService.createRoom(seaStudio1);
+        }
+
+        Room seaStudio2 = Room.builder()
+                .roomNumber(23)
+                .roomType(RoomType.STUDIO)
+                .roomView(RoomView.SEA)
+                .roomPrice(320.00)
+                .build();
+
+        if (isRoomNumberUnique(seaStudio2)) {
+            roomService.createRoom(seaStudio2);
+        }
+
+
+        //Pool Studios
+        Room poolStudio1 = Room.builder()
+                .roomNumber(24)
+                .roomType(RoomType.STUDIO)
+                .roomView(RoomView.POOL)
+                .roomPrice(320.00)
+                .build();
+
+        if (isRoomNumberUnique(poolStudio1)) {
+            roomService.createRoom(poolStudio1);
+        }
+
+        Room poolStudio2 = Room.builder()
+                .roomNumber(25)
+                .roomType(RoomType.STUDIO)
+                .roomView(RoomView.POOL)
+                .roomPrice(320.00)
+                .build();
+
+        if (isRoomNumberUnique(poolStudio2)) {
+            roomService.createRoom(poolStudio2);
+        }
+
+
+        //Garden Studios
+        Room gardenStudio1 = Room.builder()
+                .roomNumber(26)
+                .roomType(RoomType.STUDIO)
+                .roomView(RoomView.GARDEN)
+                .roomPrice(320.00)
+                .build();
+
+        if (isRoomNumberUnique(gardenStudio1)) {
+            roomService.createRoom(gardenStudio1);
+        }
+
+        Room gardenStudio2 = Room.builder()
+                .roomNumber(27)
+                .roomType(RoomType.STUDIO)
+                .roomView(RoomView.GARDEN)
+                .roomPrice(320.00)
+                .build();
+
+        if (isRoomNumberUnique(gardenStudio2)) {
+            roomService.createRoom(gardenStudio2);
+        }
+
+
+        //Apartments
+        //Sea Apartments
+        Room seaApartment1 = Room.builder()
+                .roomNumber(32)
+                .roomType(RoomType.APARTMENT)
+                .roomView(RoomView.SEA)
+                .roomPrice(520.00)
+                .build();
+
+        if (isRoomNumberUnique(seaApartment1)) {
+            roomService.createRoom(seaApartment1);
+        }
+
+        Room seaApartment2 = Room.builder()
+                .roomNumber(33)
+                .roomType(RoomType.APARTMENT)
+                .roomView(RoomView.SEA)
+                .roomPrice(520.00)
+                .build();
+
+        if (isRoomNumberUnique(seaApartment2)) {
+            roomService.createRoom(seaApartment2);
+        }
+
+        //Pool Apartment
+        Room poolApartment1 = Room.builder()
+                .roomNumber(34)
+                .roomType(RoomType.APARTMENT)
+                .roomView(RoomView.POOL)
+                .roomPrice(520.00)
+                .build();
+
+        if (isRoomNumberUnique(poolApartment1)) {
+            roomService.createRoom(poolApartment1);
+        }
+    }
+}
